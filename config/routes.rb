1bdffda423859b8b8774664a449fb03bd5321b5b
Miles McArdle-Coe <email": null>
2016-11-19 14:19:37 -0500

    saturday user log stuff complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
+  resources :users
+  resources :sessions
+
+  resources :posts do
+    resources :comments
+  end
+
   root 'pages#show'
 end
